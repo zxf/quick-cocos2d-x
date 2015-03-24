d57e748b6c984135c865f03fd220b46f13be524d
zxf <zxfnnn@gmail.com>
2015-03-24 16:04:10 +0800

    update lua_pomelo

--- quick/lib/quick-src/lua_extensions/pomelo/lua_pomelo.c
+++ quick/lib/quick-src/lua_extensions/pomelo/lua_pomelo.c
@@ -2,22 +2,71 @@
 #include <lua.h>
 #include <pomelo.h>
 
-int lua_pomelo_lib_init(lua_State* L) {
-    int log_level = 1;
+static int lib_init(lua_State* L) {
+    int log_level;
     char* ca_file = NULL;
     char* ca_path = NULL;
+    log_level = luaL_checkinteger(L, 1);
+    if(lua_gettop(L) > 0){
+        if(lua_isnil(L, 1)){
+            lua_remove(L, 1)
+        } else {
+            ca_file = luaL_checkstring(L, 1);
+        }
+    }
+    if(lua_gettop(L) > 0){
+        if(lua_isnil(L, 1)){
+            lua_remove(L, 1)
+        } else {
+            ca_path = luaL_checkstring(L, 1);
+        }
+    }
+#if !defined(PC_NO_UV_TLS_TRANS)
+    if (ca_file || ca_path) {
+        tr_uv_tls_set_ca_file(ca_file, ca_path);
+    }
+#endif
 	pc_lib_set_default_log_level(log_level); 
     pc_lib_init(NULL, NULL, NULL, "Lua Client");
-    lua_pushinteger(L, 100);
+    lua_pushnil(L);
     return 1;
 }
 
+static int lib_cleanup(lua_State* L) {
+    pc_lib_cleanup();
+    lua_pushnil(L);
+    return 1;
+}
+
+static int ev_to_str(lua_State* L) {
+    int ev = luaL_checkinteger(L, 1);
+    lua_pushstring(L, pc_client_ev_str(ev));
+    return 1;
+}
+
+static int rc_to_str(lua_State* L) {
+    int rc = luaL_checkinteger(L, 1);
+    lua_pushstring(L, pc_client_rc_str(ev));
+    return 1;
+}
+
+static int state_to_str(lua_State* L) {
+    int state = luaL_checkinteger(L, 1);
+    lua_pushstring(L, pc_client_state_str(ev));
+    return 1;
+}
+
+static int create(lua_State* L) {
+    return 1
+}
+
 static const luaL_reg pomelo_functions[] = {
-    {"lib_init",    lua_pomelo_lib_init},
+    {"lib_init",    lib_init},
     {NULL,  NULL}
 };
 
 int luaopen_pomelo(lua_State* L) {
+
     luaL_register(L, "pomelo", pomelo_functions);
     return 1;
 }
