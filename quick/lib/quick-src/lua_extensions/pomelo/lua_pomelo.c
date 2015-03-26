bbbd87ee298f2f4b2523361ad159e49135378f9b
zxf <zxfnnn@gmail.com>
2015-03-26 17:49:11 +0800

    update lua_pomole

--- quick/lib/quick-src/lua_extensions/pomelo/lua_pomelo.c
+++ quick/lib/quick-src/lua_extensions/pomelo/lua_pomelo.c
@@ -1,8 +1,9 @@
 #include <string.h>
-//#include <stdlib.h>
+
 #include <lauxlib.h>
 #include <lua.h>
 #include <pomelo.h>
+
 
 static int ext_lua_checkbool(lua_State* L, int idx) {
 	if(lua_isboolean(L, idx)){
@@ -21,12 +22,53 @@ static int ext_lua_checkbool(lua_State* L, int idx) {
 	}
 }
 
+static lua_State* ext_lua_state(lua_State* L, int idx) {
+	lua_State* lua_st;
+	if(!lua_isfunction(L, idx) || !lua_iscfunction(L, idx)) {
+		luaL_error(L, "type error:not a function");
+	} else {
+		lua_st = lua_newthread(L);
+		lua_pop(L, 1);
+		lua_pushcfunction(lua_st, lua_tocfunction(L, idx));
+	}
+	return lua_st;
+}
+
+
+static int ext_lua_reg_state(lua_State* L, void* group, lua_State* LL) {
+	lua_pushlightuserdata(L, group);
+	lua_gettable(L, LUA_REGISTRYINDEX);
+	if(lua_isnoneornil(L, -1)){
+		lua_pop(L, 1);
+		lua_newtable(L);
+		lua_pushlightuserdata(L, group);
+		lua_pushvalue(L, -2);
+		lua_settable(L, LUA_REGISTRYINDEX);
+	}
+	lua_pushlightuserdata(L, (void *)&LL);
+	lua_pushlightuserdata(L, LL);
+	lua_settable(L, -3);
+	lua_pop(L, 1);
+	return 0;
+}
+
+static int ext_lua_unreg_state(lua_State* L, void* group) {
+	lua_pushlightuserdata(L, group);
+	lua_pushnil(L);
+	lua_settable(L, LUA_REGISTRYINDEX);
+	return 0;
+}
+
+static void default_destructor(void* ex_data) {
+    
+}
+
 static int local_storage_cb(pc_local_storage_op_t op, char* data, size_t* len, void* ex_data) {
-	lua_State* L = (lua_State*)ex_data;
 	int ret = -1;
 	int res_len = 0;
     char* res = NULL;
 	const char* error;
+	lua_State* L = ext_lua_state((lua_State*)ex_data, -1);
 
 	if (op == PC_LOCAL_STORAGE_OP_WRITE) {
 		lua_pushvalue(L, -1);
@@ -136,33 +178,83 @@ static int create(lua_State* L) {
     int polling = 0;
     pc_client_t* client = NULL;
 	int ret;
+	lua_State* ls_ex_data;
 	pc_client_config_t config = PC_CLIENT_CONFIG_DEFAULT;
 	tls = ext_lua_checkbool(L, 1);
 	polling = ext_lua_checkbool(L, 1);
+
 	if (tls) {
         config.transport_name = PC_TR_NAME_UV_TLS;
     }
     if (polling) {
         config.enable_polling = 1;
     }
-
+	
 	lua_newuserdata(L, pc_client_size());
 	client = (pc_client_t*)lua_touserdata(L, -1);
-	lua_pop(L, 1);
-	//client = (pc_client_t* )malloc(pc_client_size());
 	if(!client){
 		lua_pushnil(L);
 		return 0;
 	}
-
+	
+	ls_ex_data = ext_lua_state(L, -2);
 	config.local_storage_cb = local_storage_cb;
-    config.ls_ex_data = L;
+	config.ls_ex_data = ls_ex_data;
 	ret = pc_client_init(client, NULL, &config);
     if (ret != PC_RC_OK) {
+		lua_pop(L, 2);
         lua_pushnil(L);
 		return 0;
     }
-	lua_pushinteger(L, (unsigned long)client);
+	lua_pop(L, 1);
+	ext_lua_reg_state(L, client, ls_ex_data);
+    return 0;
+}
+
+static int connect(lua_State* L) {
+    unsigned long addr;
+    pc_client_t* client;
+    char* host = NULL;
+    int port = 0;
+    int ret;
+
+	client = (pc_client_t* )lua_touserdata(L, 1);
+	host = (char*)luaL_checkstring(L, 2);
+	port = luaL_checkinteger(L, 3);
+
+    ret = pc_client_connect(client, host, port, NULL);
+
+	lua_pushinteger(L, ret);
+    return 0;
+}
+
+static int state(lua_State* L) {
+    unsigned long addr;
+    pc_client_t* client;
+    int state;
+
+    client = (pc_client_t* )lua_touserdata(L, 1);
+    
+    state = pc_client_state(client);
+
+    lua_pushinteger(L, state);
+    return 0;
+}
+
+static int add_ev_handler(lua_State* L) {
+    unsigned long addr;
+    pc_client_t* client;
+    int ret;
+	lua_State* ls_ex_data;
+
+    client = (pc_client_t* )lua_touserdata(L, 1);
+
+	ls_ex_data = ext_lua_state(L, -1);
+    ret = pc_client_add_ev_handler(client, default_event_cb, ls_ex_data, default_destructor);
+	if (ret != PC_EV_INVALID_HANDLER_ID) {
+		ext_lua_reg_state(L, client, ls_ex_data);
+	}
+	lua_pushinteger(L, ret);
     return 0;
 }
 
@@ -173,10 +265,12 @@ static const luaL_reg pomelo_functions[] = {
 	{"rc_to_str", rc_to_str},
 	{"state_to_str", state_to_str},
 	{"create", create},
+	{"connect", connect},
+	{"state", state},
     {NULL,  NULL}
 };
 
 int luaopen_pomelo(lua_State* L) {
     luaL_register(L, "luapomelo", pomelo_functions);
-    return 1;
+    return 0;
 }
