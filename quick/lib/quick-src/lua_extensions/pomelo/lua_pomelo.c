973b89cd1967076de1d3a48f05359510abeaf7ee
zxf <zxfnnn@gmail.com>
2015-03-28 12:42:34 +0800

    update lua_pomelo

--- quick/lib/quick-src/lua_extensions/pomelo/lua_pomelo.c
+++ quick/lib/quick-src/lua_extensions/pomelo/lua_pomelo.c
@@ -22,46 +22,62 @@ static int ext_lua_checkbool(lua_State* L, int idx) {
 	}
 }
 
-static lua_State* ext_lua_func_state(lua_State* L, int idx) {
+static lua_State* ext_lua_reg_state(lua_State* L){
 	lua_State* lua_st;
+	char* state_key = "__state";
 	int ref;
-	if(!lua_isfunction(L, idx) && !lua_iscfunction(L, idx)) {
-		luaL_error(L, "type error: %s is not a function.", lua_typename(L, lua_type(L, idx)));
+	lua_newtable(L);
+	lua_pushstring(L, state_key);
+	lua_st = lua_newthread(L);
+	lua_settable(L, -3);
+	ref = luaL_ref(L, LUA_REGISTRYINDEX);
+	lua_pushinteger(lua_st, ref);
+	return lua_st;
+}
+
+static int ext_lua_unreg_state(lua_State* L){
+	int ref;
+	ref = lua_tointeger(L, 1);
+	luaL_unref(L, LUA_REGISTRYINDEX, ref);
+	return 0;
+}
+
+static int ext_lua_reg_function_state(lua_State* destL, lua_State* srcL, int idx, const char* func_key){
+	int ref;
+	if(!lua_isfunction(srcL, idx) && !lua_iscfunction(srcL, idx)) {
+		luaL_error(srcL, "type error: %s is not a function.", lua_typename(srcL, lua_type(srcL, idx)));
 	} else {
-		lua_pushvalue(L, idx);
-		ref = luaL_ref(L, LUA_REGISTRYINDEX);
-		lua_st = lua_newthread(L);
-		lua_rawgeti(lua_st, LUA_REGISTRYINDEX, ref);
-		luaL_unref(L, LUA_REGISTRYINDEX, ref);
-		lua_pop(L, 1);
+		ref = lua_tointeger(destL, 1);
+		lua_pushvalue(srcL, idx);
+		lua_rawgeti(srcL, LUA_REGISTRYINDEX, ref);
+		lua_pushstring(srcL, func_key);
+		lua_pushvalue(srcL, -3);
+		lua_settable(srcL, -3);
+		lua_pop(srcL, 2);
+	}
+	return 0;
+}
+
+static lua_State* ext_lua_get_function_state(lua_State* L, const char* func_key) {
+	int ref;
+	lua_State* lua_st;
+
+	ref = lua_tointeger(L, 1);
+	lua_st = lua_newthread(L);
+	lua_pop(L, 1);
+	lua_rawgeti(lua_st, LUA_REGISTRYINDEX, ref);
+	lua_pushstring(lua_st, func_key);
+	lua_gettable(lua_st, -2);
+	if(!lua_isfunction(lua_st, -1) && !lua_iscfunction(lua_st, -1)) {
+		luaL_error(lua_st, "type error: %s is not a function.", lua_typename(lua_st, lua_type(lua_st, -1)));
+	} else {
+		lua_remove(lua_st, 1);
 	}
 
 	return lua_st;
 }
 
-static int ext_lua_reg_state(lua_State* L, void* group, lua_State* LL) {
-	lua_pushlightuserdata(L, group);
-	lua_gettable(L, LUA_REGISTRYINDEX);
-	if(lua_isnoneornil(L, -1)){
-		lua_pop(L, 1);
-		lua_newtable(L);
-		lua_pushlightuserdata(L, group);
-		lua_pushvalue(L, -2);
-		lua_settable(L, LUA_REGISTRYINDEX);
-	}
-	lua_pushlightuserdata(L, (void *)&LL);
-	lua_pushlightuserdata(L, LL);
-	lua_settable(L, -3);
-	lua_pop(L, 1);
-	return 0;
-}
 
-static int ext_lua_unreg_state(lua_State* L, void* group) {
-	lua_pushlightuserdata(L, group);
-	lua_pushnil(L);
-	lua_settable(L, LUA_REGISTRYINDEX);
-	return 0;
-}
 
 static void default_destructor(void* ex_data) {
     
@@ -71,7 +87,7 @@ static int local_storage_cb(pc_local_storage_op_t op, char* data, size_t* len, v
 	int ret = -1;
     char* res = NULL;
 	const char* error;
-	lua_State* L = ext_lua_func_state((lua_State*)ex_data, -1);	
+	lua_State* L = ext_lua_get_function_state((lua_State*)ex_data, "local_storage_cb");	
 	if (op == PC_LOCAL_STORAGE_OP_WRITE) {
 		lua_pushinteger(L, op);
 		lua_pushlstring(L, data, *len);
@@ -189,16 +205,17 @@ static int create(lua_State* L) {
 		return 0;
 	}
 	
-	ls_ex_data = ext_lua_func_state(L, -2);
+	ls_ex_data = ext_lua_reg_state(L);
+	ext_lua_reg_function_state(ls_ex_data, L, -2, "local_storage_cb");
 	config.local_storage_cb = local_storage_cb;
 	config.ls_ex_data = ls_ex_data;
 	ret = pc_client_init(client, NULL, &config);
     if (ret != PC_RC_OK) {
+		ext_lua_unreg_state(ls_ex_data);
 		lua_pop(L, 1);
         lua_pushnil(L);
 		return 0;
     }
-	ext_lua_reg_state(L, client, ls_ex_data);
     return 0;
 }
 
@@ -241,7 +258,7 @@ static int add_ev_handler(lua_State* L) {
 
     client = (pc_client_t* )lua_touserdata(L, 1);
 
-	ls_ex_data = ext_lua_state(L, -1);
+	ls_ex_data = client->config.ls_ex_data;
     ret = pc_client_add_ev_handler(client, default_event_cb, ls_ex_data, default_destructor);
 	if (ret != PC_EV_INVALID_HANDLER_ID) {
 		ext_lua_reg_state(L, client, ls_ex_data);
